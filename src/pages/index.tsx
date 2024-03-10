ccc592c641984bec1bfb5d4fe73c55d11a3b5082
junhyeon0218 <junhyeon0218@daum.net>
2024-03-10 22:45:23 +0900

    초기세팅

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div>
+      <div>dsafdsafdsaf</div>dsafsafdasfdsafnkjnsafkjsanfkjsanfkjanfjkanfkjnaskfjnas;kfjnakvjnakvjndsa Lorem ipsum dolor
+      sit amet consectetur adipisicing elit. Repellendus numquam aut totam odit est. Eveniet non assumenda distinctio
+      iste commodi officiis eos libero doloribus, exercitationem maiores. Voluptas dignissimos quaerat aliquid.
+    </div>
+  );
+}
