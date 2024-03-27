afe25dcb0b29c1d34d74370d4ff264fa4f595284
ysh73900 <74593476+ysh73900@users.noreply.github.com>
2024-03-27 20:33:33 +0900

    🎨 Design: 메인페이지 반응형 ui 구현 (#6)
    Co-authored-by: LeeSeungHoon <ysh73900@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
-import Header from '@/components/common/header';
 import Content from '@/components/landing/content';
 import Footer from '@/components/landing/footer';
 
 export default function main() {
   return (
     <div>
-      <Header />
       <Content />
       <Footer />
     </div>
